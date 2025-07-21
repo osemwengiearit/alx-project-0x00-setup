2778a1172fa30b32fc5edba02defab3969022bac
Uyearit <uyearit23@gmail.com>
2025-07-21 19:43:52 +0100

    Use Card and Pill components on landing page with dynamic props

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,7 +1,17 @@
+import React from 'react';
+import Card from '@/components/Card';
+
 const Landing: React.FC = () => {
   return (
-    <div>
-      <h1 className="text-xl font-extralight">Landing Page</h1>
+    <div className="p-4">
+      <h1 className="text-xl font-extralight mb-4">Landing Page</h1>
+
+      {/* Render Cards */}
+      <div className="flex flex-wrap gap-6">
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
   );
 };
